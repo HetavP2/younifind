64023749da7c02ad7dc2b2bf4da488ab2a282230
HetavP2 <hetav.j.patel@gmail.com>
2023-12-25 11:05:29 -0500

    ready for vercel

--- app/(site)/components/HomePageContent.tsx
+++ app/(site)/components/HomePageContent.tsx
@@ -1,7 +1,7 @@
 export default function HomePageContent() {
   return (
     <div>
-      <section className="bg-white dark:bg-gray-900">
+      {/* <section className="bg-white dark:bg-gray-900">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-royalyellow">
@@ -665,7 +665,7 @@ export default function HomePageContent() {
             </div>
           </div>
         </div>
-      </footer>
+      </footer> */}
     </div>
   );
 }
