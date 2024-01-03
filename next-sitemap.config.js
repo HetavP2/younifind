2fa5ec56bc727506fbab62480aba01df56f9a3b5
HetavP2 <hetav.j.patel@gmail.com>
2024-01-03 14:26:58 -0500

    work plz

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,24 +1,24 @@
-// module.exports = {
-//   siteUrl: "https://younifind.ca",
-//   generateRobotsTxt: true, // (optional)
-//   robotsTxtOptions: {
-//     policies: [
-//       {
-//         userAgent: "*",
-//         allow: "/",
-//       },
-//       {
-//         userAgent: "*",
-//         disallow: ["/adminPanel"],
-//       },
-//     ],
-//   },
-// };
-
-/** @type {import('next-sitemap').IConfig} */
-
-const dev = process.env.NODE_ENV !== 'production';
-
 module.exports = {
-  siteUrl: "https://younifind.ca"
+  siteUrl: "https://younifind.ca",
+  generateRobotsTxt: true, // (optional)
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        disallow: ["/adminPanel"],
+      },
+    ],
+  },
 };
+
+// /** @type {import('next-sitemap').IConfig} */
+
+// const dev = process.env.NODE_ENV !== 'production';
+
+// module.exports = {
+//   siteUrl: "https://younifind.ca"
+// };
