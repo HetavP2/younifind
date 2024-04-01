6005a581a3945883ae8d04801a2d6d16872f2213
HetavP2 <hetav.j.patel@gmail.com>
2024-04-01 15:50:20 -0400

    stash

--- actions/opportunity/add-opp/addOpportunity.ts
+++ actions/opportunity/add-opp/addOpportunity.ts
@@ -142,6 +142,10 @@ const addOpportunity = async ({
   
 
   if (!approved) {
+    const { error } = await supabase
+      .from("opportunity_statuses").update({ approved: false }).eq("opportunity_id", id);
+    
+    
     emailSentStatus = await sendEmail({
       to: ["hetav.j.patel@gmail.com", "vangara.anirudhbharadwaj@gmail.com"],
       subject: "Please Approve Opportunity",
