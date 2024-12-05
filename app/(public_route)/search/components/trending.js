f9fec6c2f44974a9d7fc5ced100cef403618e88c
HetavP2 <hetav.j.patel@gmail.com>
2024-12-04 20:42:21 -0500

    trending data update

--- app/(public_route)/search/components/trending.js
+++ app/(public_route)/search/components/trending.js
@@ -1,7 +1,7 @@
 // trending.js
 const trendingdata = [
-    { id: 1, name: "TKS Program", website: "https://www.tks.world/", tag1: "Internship", tag2: "Paid" },
-    { id: 1, name: "SHAD Valley", website: "https://www.shad.ca/", tag1: "Summer", tag2: "Paid    " },
+    // { id: 1, name: "TKS Program", website: "https://www.tks.world/", tag1: "Internship", tag2: "Paid" },
+    // { id: 1, name: "SHAD Valley", website: "https://www.shad.ca/", tag1: "Summer", tag2: "Paid    " },
     { id: 1, name: "Sunnybrook Ultrasound", website: "https://sunnybrook.ca/research/content/?page=sri-ed-summ-ultra", tag1: "Research", tag2: "Paid" },
     { id: 1, name: "Relay For Life", website: "https://cancer.ca/en/get-involved/our-events/relay-for-life", tag1: "Fundraiser", tag2: "Free" },
     { id: 1, name: "NDP Canvasser", website: "https://www.ontariondp.ca/node/9636", tag1: "Internship", tag2: "Paid" }
